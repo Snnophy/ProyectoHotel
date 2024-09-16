b31086197813967087e7125599cad07f41176912
smith <smith@DESKTOP-1E2DK7B>
2024-09-16 12:21:48 -0500

    Agregar archivos de proyecto.

---
+++ Logica/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-1E2DK7B\\SQLEXPRESS01;Initial Catalog=DB_HOTELXYZ;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+
+    }
+}
